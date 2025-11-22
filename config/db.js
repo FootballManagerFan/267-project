d6cf9e96158c4c54ddafa55b14337fc2dafd05bb
FootballManagerFan <maxpjax@gmail.com>
2025-11-22 18:54:18 -0500

    added env functions

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '$10Summer2004!',
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME || 'registration_db',
   port: Number(process.env.DB_PORT) || 3306,
   waitForConnections: true,
